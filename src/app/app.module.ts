f978147fb4df40fefc8a0152ebfa24050ad9a60a
Arthur Audenaert <Gatarlo@utlook.fr>
2021-06-27 03:37:27 +0200

    home page - missing particules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgParticlesModule} from "ng-particles";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ParticlesComponent } from './particles/particles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    ParticlesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/particles/particles.component.ts
@@ -0,0 +1,136 @@
+import { Component, OnInit } from '@angular/core';
+import { Container, Main } from 'tsparticles';
+
+@Component({
+  selector: 'app-particles',
+  templateUrl: './particles.component.html',
+  styleUrls: ['./particles.component.scss']
+})
+
+export class ParticlesComponent implements OnInit {
+
+  id = "tsparticles";
+  
+  particlesOptions  = {
+    particles: {
+      number: {
+        value: 8,
+        density: {
+          enable: true,
+          value_area: 1959
+        }
+      },
+      color: {
+        value: '#cdc87e'
+      },
+      shape: {
+        type: 'polygon',
+        stroke: {
+          width: 0.5,
+          color: "#ffffff"
+        },
+        polygon: {
+          nb_side: 6
+        }
+      },
+      opacity: {
+        value: 0.5,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 0.1,
+          opacity_min: 0.5,
+          sync: false
+        }
+      },
+      size: {
+        value: 100,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 10,
+          size_min: 40,
+          sync: false
+        }
+      },
+      line_linked: {
+        enable: true,
+        distance: 300,
+        color: "#FFFFFF",
+        opacity: 1,
+        width: 2
+      },
+      move: {
+        enable: true,
+        speed: 3,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: false,
+          mode: "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 4
+        },
+        remove: {
+          particles_nb: 2
+        }
+      }
+    },
+    retina_detect: true
+  };
+  
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
+
+  particlesInit(main: Main): void {
+    console.log(main);
+    
+    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+  }
+}
